44850eab1026c2bf450890c2125c79eff789df6c
kasturipratik <pratik.parajuli@yahoo.com>
2018-07-29 22:57:12 -0400

    added comments on the java classes

--- src/com/company/Student.java
+++ src/com/company/Student.java
@@ -5,10 +5,11 @@ import java.util.Map;
 
 public class Student {
 
-
+    /*declare variable student name and their id*/
     String name;
     int id;
 
+    /*getter and setter for the student*/
     public String getName() {
         return name;
     }
@@ -25,13 +26,21 @@ public class Student {
         this.id = id;
     }
 
+    /*method to concatenate all the student into a string and return it*/
     public String toString(HashMap<Integer,String > student){
+
+        /*declaring empty string*/
         String listOfStudent="";
+
         /*looping through hashmap to print both  */
         for(Map.Entry<Integer,String> l :student.entrySet()){
+
             //concatenating string from the hashmap to return list of students with id and name
             listOfStudent = listOfStudent + "\n" +l.getKey() + " " + l.getValue();
+            /*previous name and id of sudent*/ /*getting the key and value of the hashmap*/
         }
+
+        /*returning the string of all the student list*/
         return  listOfStudent;
     }
 }
